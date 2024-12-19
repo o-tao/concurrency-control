fb2831a52307203831967f84e4e282754648277b
o-tao <dhtkdals369@gmail.com>
2024-12-19 20:34:15 +0900

    오타 수정

--- src/main/java/app/concurrencycontrol/domain/repository/CouponRepository.java
+++ src/main/java/app/concurrencycontrol/domain/repository/CouponRepository.java
@@ -25,6 +25,6 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
 /* Pessimistic: LockModeType
  *
  * PESSIMISTIC_READ: dirty read가 발생하지 않을 때마다 공유 락을 획득하여 데이터가 수정/삭제 됨을 방지
- * PESSIMISTIC_WRTE: 배타적 락을 획득하여 다른 Transactional에서 조회/수정/삭제하는 것을 방지
+ * PESSIMISTIC_WRITE: 배타적 락을 획득하여 다른 Transactional에서 조회/수정/삭제하는 것을 방지
  * PESSIMISTIC_FORCE_INCREMENT: PESSIMISTIC_WRITE와 비슷하지만 @Version 어노테이션이 있는 Entity와 협력하기 위해 도입, Lock을 획득하면 Version 업데이트
  * */
